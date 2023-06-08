8607efc71e3c14dc8fb13f843d791a7f110829aa
Ragu Subaramaniyam <raguct25@gmail.com>
2023-06-08 17:48:23 +0530

    project configuration

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main>
+      <div className="flex h-screen justify-center items-center">
+        <div className="text-center text-2xl text-rose-500">
+          ReactJs
+          <h1 className="text-3x">Welcome NextJs Project!</h1>
+          <p className="text-sm italic">Learn PoC</p>
+        </div>
+      </div>
+    </main>
+  );
+}
